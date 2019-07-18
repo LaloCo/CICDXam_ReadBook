1258ee6af3b59434e1d76dd8bd9ca0eaa77cabad
Eduardo Rosas <lalorosas@outlook.com>
2019-07-18 08:03:45 -0500

    Solve #6

--- ReadBooks/App.xaml.cs
+++ ReadBooks/App.xaml.cs
@@ -16,12 +16,18 @@ namespace ReadBooks
             MainPage = new NavigationPage(new MainPage());
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
             // Handle when your app starts
             AppCenter.Start("ios=ea6fc345-1671-4945-9fbe-7cc344cda0d8;" +
                             "android=ea623267-b35a-495a-9cf7-c5775ff56f78",
                             typeof(Analytics), typeof(Crashes));
+
+            bool didAppCrash = await Crashes.HasCrashedInLastSessionAsync();
+            if(didAppCrash)
+            {
+                await MainPage.DisplayAlert("I'm sorry", "It appears we had some issue there, sorry about that.", "Don't worry");
+            }
         }
 
         protected override void OnSleep()
